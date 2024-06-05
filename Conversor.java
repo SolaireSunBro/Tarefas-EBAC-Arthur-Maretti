30fe77891a96d4b89b1b0e1a18395c2d3be9f1b3
SolaireSunBro <160795039+SolaireSunBro@users.noreply.github.com>
2024-06-05 10:32:48 -0300

    Add files via upload

---
+++ Conversor.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Conversor {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número \n");
+        int n = sc.nextInt();
+
+        Integer n1 = n;
+
+        System.out.println("Número Digitado " + n1 + "\n");
+        System.out.println("Número convertido " + n1 + "\n");
+    }
+
+
+
+}
