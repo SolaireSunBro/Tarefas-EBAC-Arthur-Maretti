1396f1a161a173ce7d443cc254ffb5bb3825b8d7
ArthurMaretti <160795039+SolaireSunBro@users.noreply.github.com>
2026-01-06 19:56:14 -0300

    Add files via upload

---
+++ Mod43/ProdutoService/ProdutoService/src/main/java/com/br/arthas/ProdutoService/repository/IProdutoRepository.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.br.arthas.ProdutoService.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.br.arthas.ProdutoService.domain.Produto;
+import com.br.arthas.ProdutoService.domain.Produto.Status;
+
+
+/**
+ * @author rodrigo.pires
+ *
+ */
+@Repository
+public interface IProdutoRepository extends MongoRepository<Produto, String>{
+
+	Optional<Produto> findByCodigo(String codigo);
+	
+	Page<Produto> findAllByStatus(Pageable pageable, Status status);
+}
